3f61cc90ad90111626c20357ba0eff44dbab6fc9
Luis Sanjuan <luiscode92@gmail.com>
2022-08-16 12:27:39 -0500

    Frontend code

---
+++ utils/connectContract.js
@@ -0,0 +1,26 @@
+import abiJSON from "./Web3RSVP.json";
+import { ethers } from "ethers";
+
+function connectContract() {
+    //Note: Your contractAddress will start with 0x, delete everything between the quotes and paste your contract address.
+    const contractAddress = "0xc7a9a2a2Cf58816De1eF30A399F451a9E7700B1c";
+    const contractABI = abiJSON.abi;
+    let rsvpContract;
+    try {
+      const { ethereum } = window;// this is the global etheroum api
+  
+      if (ethereum) {
+        //checking for eth object in the window
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log("ERROR:", error);
+    }
+    return rsvpContract;
+  }
+  
+  export default connectContract;
